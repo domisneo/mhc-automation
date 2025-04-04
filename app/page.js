d94effe5675f77160a8634d75d131f1c382d2d73
Dom <domcomp2023@Doms-MBP.attlocal.net>
2025-04-04 02:06:14 -0700

    Add dashboard as homepage

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>🚀 MHC Automation App is Live!</h1>
+      <p>More features coming soon. You're successfully deployed on Vercel.</p>
+    </div>
+  );
+}
