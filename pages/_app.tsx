d16710e0939a9f983d078155ad33622ff5c840aa
Dom <domcomp2023@Doms-MBP.attlocal.net>
2025-04-04 02:42:40 -0700

    Removed pages folder to use App Router

--- pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import "❌ TypeScript → No  ✅ ESLint → Yes  ✅ Tailwind → Yes  ❌ Use `src/` folder → No  ✅ App Router → Yescd mhc-automationcode .@/styles/globals.css";
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
